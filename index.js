935cdaadee96334f08e9d4a2aebd09652ad16dbb
prasad217 <cr6473@srmist.edu.in>
2023-11-11 13:52:51 +0530

    no errors

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from 'inquirer';
+import qr from 'qr-image'
+import fs from 'fs'
+inquirer
+  .prompt([
+    {
+        message: "Type in Your URL:",
+        name: "URL",
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+ 
+   var qr_svg = qr.image(url)
+   qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+   fs.writeFile("URL.txt",url, (err) => {
+    if (err) throw err;
+    console.log("The File has been saved sucessfully")
+   });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
